c865dd35d66bc5e4525612d24ecfeaad66628304
Ejaz47-Hub <ejaz47026@gmail.com>
2025-07-05 09:30:50 +0530

    React

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const Newsletter = () => {
+  const onSubmitHandler = (event) =>{
+    event.preventDefault();
+  }
+
   return (
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe Now & Get 20% Off</p>
         <p className='text-gray-600 mt-3'>
             "Wear the Moment. Live Forever."
         </p>
+      <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 rounded-3xl overflow-hidden'>
+        <input type="email" placeholder='Enter Your Email' className='w-full sm:flex-1 outline-none' required />
+        <button type='Submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
+      </form>
     </div>
   )
 }
